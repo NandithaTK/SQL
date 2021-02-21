6cb2d9f456c8b403cd7f55288026c81bf8e176a6
NandithaTK <75018989+NandithaTK@users.noreply.github.com>
2021-02-21 13:05:17 +0530

    adding the sql file for operators topic

---
+++ sqltask.sql
@@ -0,0 +1,27 @@
+
+show databases;
+
+
+use company_data_db;
+
+
+ desc department;
+
+
+desc emp;
+   
+
+ select * from department;
+
+select * from emp;
+
+
+select * from emp where salary<1000;
+
+
+select * from emp where salary>=3000;
+
+select * from emp where departNo=10;
+
+select * from emp where departNo IN(30,40,50);
+
